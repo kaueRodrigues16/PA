2e0549c386aba28d5ff5732f98662a82749494b2
kaueRodrigues16 <142915945+kaueRodrigues16@users.noreply.github.com>
2023-09-19 12:15:49 -0300

    Add files via upload

---
+++ doWhile/Idade.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Idade {
+
+public static void main(String[] args) {
+		
+		Scanner ler = new Scanner (System.in);
+		
+		int nascimento, AnoAtual,idade;
+		
+		
+		do {
+		
+		System.out.println("coloque seu ano ano atual");
+		    
+		AnoAtual=ler.nextInt();
+		
+		System.out.println("coloque sua data de nascimensto");
+	
+		nascimento=ler.nextInt();
+		
+		idade=AnoAtual-nascimento;
+		
+			if(idade<=18) {
+				
+				System.out.println("como sua idade é "+idade+" voce é menor de idade ");
+			}
+			
+			else {
+				
+				System.out.println("como sua idade é "+idade+" voce é maior de idade ");
+			}
+			System.out.println("como sua idade é "+idade+" voce é maior de idade ");
+			
+		} while()
+}
+}
+
---
+++ doWhile/ZeroAZequinho.java
@@ -0,0 +1,21 @@
+
+public class ZeroAZequinho {
+
+	public static void main(String[] args) {
+		
+		
+		int i=0;
+		
+		do {
+			
+			System.out.println(i);
+			
+			
+			
+			i=i+2;	
+		}
+		while(i<=500);
+
+	}
+
+}
