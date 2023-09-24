7e69bb51563ad521b2d7d778211d5d50b0eabdaf
kaueRodrigues16 <142915945+kaueRodrigues16@users.noreply.github.com>
2023-09-24 18:02:30 -0300

    Add files via upload

--- doWhile/Idade.java
+++ doWhile/Idade.java
@@ -1,37 +1,55 @@
+package Exercicio;
 import java.util.Scanner;
 public class Idade {
 
+	
 public static void main(String[] args) {
 		
 		Scanner ler = new Scanner (System.in);
+		 
 		
-		int nascimento, AnoAtual,idade;
-		
+	int AnoAtual, nascimento, idade, i=0;
+
+	
 		
 		do {
-		
-		System.out.println("coloque seu ano ano atual");
-		    
-		AnoAtual=ler.nextInt();
-		
-		System.out.println("coloque sua data de nascimensto");
-	
-		nascimento=ler.nextInt();
-		
-		idade=AnoAtual-nascimento;
-		
-			if(idade<=18) {
-				
-				System.out.println("como sua idade é "+idade+" voce é menor de idade ");
-			}
+			System.out.println("coloque seu ano ano atual");
+			    
+			AnoAtual=ler.nextInt();
 			
-			else {
-				
-				System.out.println("como sua idade é "+idade+" voce é maior de idade ");
-			}
-			System.out.println("como sua idade é "+idade+" voce é maior de idade ");
+			System.out.println("coloque sua data de nascimensto");
+		
+			nascimento = ler.nextInt();
 			
-		} while()
-}
+			idade= AnoAtual-nascimento;
+			
+				if(idade<=18) {
+					
+					System.out.println("como sua idade Ã© "+idade+" voce Ã© menor de idade ");
+				}
+				
+				else {
+					
+					System.out.println("como sua idade Ã© "+idade+" voce Ã© maior de idade ");
+				}
+			
+				System.out.println("Deseja continuar o codigo");
+				
+				
+				i= ler.nextInt();
+			
+				
+		} 
+
+		while(i!=2) ;
+
+
+		System.out.println("Muito obrigado");
+		
+
+
 }
 
+
+
+}
