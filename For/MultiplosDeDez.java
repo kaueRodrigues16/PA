518b0d02bba7978b4fb71bb56773a4c600c2fb61
kaueRodrigues16 <142915945+kaueRodrigues16@users.noreply.github.com>
2023-09-24 18:42:39 -0300

    Add files via upload

---
+++ For/MultiplosDeDez.java
@@ -0,0 +1,23 @@
+package Exercicio;
+
+public class MultiplosDeDez {
+
+public static void main(String[] args) {
+		
+		
+		int i;
+		
+		
+		for(i=1;i<101;i++ );
+		 System.out.println(i);
+   if(i%10==i) {
+
+	   System.out.println(i+" é multilo de 10");
+   }
+	   else{
+	
+		 System.out.println(i);  
+	   }	   
+
+}
+}
