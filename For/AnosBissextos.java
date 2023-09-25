24c798d2dd0df07453ab5dc79bc6d432184027e4
kaueRodrigues16 <142915945+kaueRodrigues16@users.noreply.github.com>
2023-09-25 19:23:20 -0300

    Add files via upload

---
+++ For/AnosBissextos.java
@@ -0,0 +1,21 @@
+package Exercicio;
+
+public class AnosBissextos {
+
+	public static void main (String[] args ) {
+	
+		int i;
+		
+		for(i=0;i!=2023;i++) {
+		
+			if(i%4==0) {
+				System.out.println(i+" é ano bissexto");			
+               }
+			else {
+			System.out.println(i+" é um ano normal");
+			}		
+			
+			
+}
+}
+}
