d05e9a9ff67714de0b78999e249dc37ea95c4527
kaueRodrigues16 <142915945+kaueRodrigues16@users.noreply.github.com>
2023-10-17 12:09:22 -0300

    Add files via upload

---
+++ Vetor/VetorAoQuadrado.java
@@ -0,0 +1,30 @@
+package Exercicio;
+import java.util.Scanner;
+public class VetorAoQuadrado {
+
+	public static void main (String [] args) {
+	 
+		Scanner in = new Scanner (System.in);
+		
+		int a[]= new int [10], i;
+		
+	
+		
+		for(i=0; i<10; i++) {
+		
+		System.out.println("coloque o " +(i+1)+ "º numero");
+		
+		a[i]=in.nextInt();
+		
+		}
+		
+		System.out.print("[a] = [ ");
+		for(i=0; i<10; i++) {
+		
+		System.out.print(a[i]*a[i] );	
+			
+			}
+       System.out.print(" ]");
+	      
+	}
+}
